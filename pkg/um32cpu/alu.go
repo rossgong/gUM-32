4615e21be54faf2cc18c7c0fe1b1940656b9dcf5
Ross Gongaware <ross@gongaware.org>
2022-01-09 17:31:41 -0500

    Refactor alu into cpu package
    There was no reason not to just include the alu functions into the cpu
    packlage

--- pkg/um32cpu/alu.go
+++ pkg/um32cpu/alu.go
@@ -1,14 +1,10 @@
-package um32alu
-
-import (
-	"gongaware.org/gUM-32/pkg/um32cpu"
-)
+package um32cpu
 
 //See notes.txt for personal notes documenting the spec
 
 //Operator #0. Conditional Move
 //IF C!=0:B->A
-func Move(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Move(a *Platter, b *Platter, c *Platter) {
 	if *c > 0 {
 		*a = *b
 	}
@@ -17,33 +13,33 @@ func Move(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
 //Operator #1. Array Index
 //B[C]->A
 //Fails if Array B doesn't exist
-func Index(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Index(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement Arrays
 }
 
 //Operator #2. Array Amendendment
 //A[B] = C
 //Fails if Array B doesn't exist
-func Amend(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Amend(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement Arrays
 }
 
 //Operator #3. Addition
 //Spec doesn't specify whether to do anything with carry so ignore overflow
-func Add(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Add(a *Platter, b *Platter, c *Platter) {
 	*a = *b + *c
 }
 
 //Operator #4. Multiplication
 //Spec doesn't specify whether to do anything with carry so ignore overflow
-func Multiply(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Multiply(a *Platter, b *Platter, c *Platter) {
 	*a = *b * *c
 }
 
 //Operator #5. Division
 //B/C->A if B/C exists
 //Fail if division by zero
-func Division(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Division(a *Platter, b *Platter, c *Platter) {
 	if *c == 0 {
 		//TODO: Fail
 	} else {
@@ -53,26 +49,26 @@ func Division(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
 
 //Operator #6. NAND
 //B NAND C -> A
-func NAnd(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func NAnd(a *Platter, b *Platter, c *Platter) {
 	*a = ^(*b & *c)
 }
 
 //Operator #7. Halt
 //Stops machine
-func Halt(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Halt(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement stops
 }
 
 //Operator #8. Allocation
 //Creates a new array with capacity c initalized with 0's
 //Seets B with a unique identifier
-func Allocate(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Allocate(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement arrays
 }
 
 //Operator #9. Abdonment
 //Deallocate array C
-func Abandon(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Abandon(a *Platter, b *Platter, c *Platter) {
 	if *c == 0 {
 		//TODO: Fail
 	}
@@ -81,7 +77,7 @@ func Abandon(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
 
 //Operator #10. Output
 //display character in register C
-func Output(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Output(a *Platter, b *Platter, c *Platter) {
 	if *c > 255 {
 		//TODO: Fail
 	}
@@ -90,18 +86,18 @@ func Output(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
 
 //Operator #11. Input
 //Takes input from console into C. If it is end of input, C is filled with 1s
-func Input(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Input(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement inputs
 }
 
 //Operator #12. Load Program
 //Load array B into program array (0) and set offset to C
-func Load(a *um32cpu.Platter, b *um32cpu.Platter, c *um32cpu.Platter) {
+func Load(a *Platter, b *Platter, c *Platter) {
 	//TODO: Implement arrays
 }
 
 //Operator #13. Orthography
 //Set a to be value provided by opcode
-func Ortho(a *um32cpu.Platter, v uint32) {
+func Ortho(a *Platter, v uint32) {
 	*a = v
 }
