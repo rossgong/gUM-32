ebfc77d6e265d901bcce6d46400607e2c1ab4b51
Ross Gongaware <ross@gongaware.org>
2022-01-12 17:29:10 -0500

    Implement basic file parser

---
+++ pkg/um32parser/parser.go
@@ -0,0 +1,25 @@
+package um32parser
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"gongaware.org/gUM-32/pkg/um32cpu"
+)
+
+func Parse(file *os.File) ([]um32cpu.Platter, error) {
+	fileStats, err := file.Stat()
+	if err == nil {
+		fileSize := fileStats.Size()
+		if fileSize%4 == 0 {
+			program := make([]um32cpu.Platter, fileSize/4)
+			err = binary.Read(file, binary.BigEndian, &program)
+
+			return program, err
+		} else {
+			return nil, fmt.Errorf("this file appears to have a bad file size")
+		}
+	}
+	return nil, err
+}
